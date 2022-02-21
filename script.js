149ffb0bc5e9372bceab758335932ac6314d46a1
Peyman Rahmani <rahmani.peyman9@gmail.com>
2022-02-21 11:48:57 +0330

    add project

---
+++ script.js
@@ -0,0 +1,13 @@
+var $ = document;
+
+var arr = [];
+
+for(let i = 0 ; i < 100 ; i++){
+     arr.push("Hello " + i);
+};
+
+console.log(arr);
+
+
+var chan = "This is Change";
+chan = "Hello Change";
